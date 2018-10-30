2d7a2ed0a26a8a669c241a19619c1c766a5fda79
Noah Z <zeilmannnoah@gmail.com>
2018-10-29 22:06:40 -0500

    Added login form

--- register.php
+++ register.php
@@ -12,9 +12,42 @@
                 </p>
                 <p>
                     <label for='loginPassword'>Password</label>
-                    <input id='loginPassword' name='loginPassword' type='password' required/>
+                    <input id='loginPassword' name='loginPassword' type='password' placeholder='Your password' required/>
                 </p>
                 <button type='submit' name='loginButton'>LOG IN</button>
+            </form>
+
+            <form id='registerForm' action='register.php' method='POST'>
+                <h2>Create your free account</h2>
+                <p>
+                    <label for='username'>Username</label>
+                    <input id='username' name='username' type='text' placeholder='e.g. bartSimpson' required/>
+                </p>
+                <p>
+                    <label for='firstName'>First name</label>
+                    <input id='firstName' name='firstName' type='text' placeholder='e.g. Bart' required/>
+                </p>
+                <p>
+                    <label for='lastName'>Last name</label>
+                    <input id='lastName' name='lastName' type='text' placeholder='e.g. Simpson' required/>
+                </p>
+                <p>
+                    <label for='email'>Email</label>
+                    <input id='email' name='email' type='email' placeholder='e.g. bart@gmail.com' required/>
+                </p>
+                <p>
+                    <label for='email2'>Confirm email</label>
+                    <input id='email2' name='email2' type='email' placeholder='e.g. bart@gmail.com' required/>
+                </p>
+                <p>
+                    <label for='password'>Password</label>
+                    <input id='password' name='password' type='password' placeholder='Your password' required/>
+                </p>
+                <p>
+                    <label for='password2'>Confirm password</label>
+                    <input id='password2' name='password2' type='password' placeholder='Your password' required/>
+                </p>
+                <button type='submit' name='registerButton'>SIGN UP</button>
             </form>
         </div>
     </body>
